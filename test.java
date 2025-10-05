9686e27d4b189fc47e7336c26847753632670f4f
Zhafranabimata <adyaganteng981@gmail.com>
2025-10-05 07:57:08 +0700

    merubah code

--- test.java
+++ test.java
@@ -1,18 +1,5 @@
-import java.util.Scanner;
-
 public class test {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double jarak, liter, hargaPerLiter, biaya;
-
-        System.out.print("Masukkan jarak tempuh (km): ");
-        jarak = input.nextDouble();
-
-        liter = jarak / 2.0;
-        hargaPerLiter = 10000;
-        biaya = liter * hargaPerLiter;
-        
-        System.out.println("Biaya bensin untuk perjalanan = Rp " + biaya);
-        input.close();
+        System.out.println("Ini Fitur 1");
     }
 }
