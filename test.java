ea24d2fce3c33bb94dcf67eb8863d36187c38b5d
Zuyabi <zhafranabimata@gmail.com>
2025-10-05 08:05:09 +0700

    Update baru coy

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Ini Fitur 1");
         System.out.println("Ini Fitur 2");
+        System.out.println("Ini Fitur 2");
     }
 }
